2e2062d9a35af891a29bfa1c600581fb851d88b8
Marquitoz <markst1811@gmail.com>
2025-10-08 11:09:46 -0500

    auth layout and main layout added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'enviroment-upeu-frontend';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './layout/main/main.component';
+import { HomeComponent } from './home/home.component';
+import { AuthLayoutComponent } from './features/auth-layout/auth-layout.component';
+import { LoginComponent } from './features/auth-layout/login/login.component';
+import { RegisterComponent } from './features/auth-layout/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: AuthLayoutComponent,
+        children: [
+        { path: '', redirectTo: 'login', pathMatch: 'full' },
+        { path: 'login', component: LoginComponent },
+        { path: 'register', component: RegisterComponent },
+        ]
+    },
+
+    {
+        path: 'main',
+        component: MainComponent,
+        children: [
+        { path: 'home', component: HomeComponent },
+        ],
+    },
+
+    { path: '**', redirectTo: '' },
+];
